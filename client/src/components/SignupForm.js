f5bd464dfa1b598a9857f381e0cc6c68eaf4ae6f
Stebbi <stefodd@yahoo.com>
2022-07-03 16:44:10 -0500

    users can now add, delete, search,sign-in/login/logout

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -32,7 +32,7 @@ const SignupForm = () => {
     }
 
     try {
-      const { data } = await createUser({
+      const { data } = await addUser({
         variables: { ...userFormData }
       });
       Auth.login(data.addUser.token);
